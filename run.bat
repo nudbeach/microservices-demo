64b4ee6d57a9931b0ba69a8a55f22de311fa730b
Administrator <Administrator@iZ6wed61e47yz1Z>
2019-11-08 11:32:18 +0800

    .

--- run.bat
+++ run.bat
@@ -3,14 +3,15 @@
 Rem --
 
 Set JAVA_HOME=c:\Java\jdk1.8.0_202\
-Set OPT1=
+Set VERSION=2.0.0.RELEASE
+Set OPT1=-Deureka.url=http://localhost:1111/eureka/ -Deureka.host=localhost
 
-start java -jar target/microservices-demo-2.0.0.RELEASE.jar registration %OPT1%
+start java %OPT1% -jar target/microservices-demo-%VERSION%.jar registration
 
 timeout /t 10 /nobreak
 
-start java -jar target/microservices-demo-2.0.0.RELEASE.jar accounts
+start java %OPT1% -jar target/microservices-demo-%VERSION%.jar accounts
 
 timeout /t 5 /nobreak
 
-start java -jar target/microservices-demo-2.0.0.RELEASE.jar web
+start java %OPT1% -jar target/microservices-demo-%VERSION%.jar web
